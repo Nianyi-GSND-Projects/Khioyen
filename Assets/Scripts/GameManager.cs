9ffe784cd7b390b56ecede6429623825f97aaa43
Nianyi Wang <wangnianyi2001@outlook.com>
2025-10-11 20:31:59 -0400

    Reorganize assets

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,240 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LongLiveKhioyen
+{
+	public static class GameManager
+	{
+		#region Life cycle
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+		static void OnGameStart()
+		{
+			/* Local data */
+			ReadSettings();
+			ReadSavegamePaths();
+
+			/* Built-in resources */
+			buildingDefinitionSheet = UnityEngine.Object.Instantiate(Resources.Load<BuildingDefinitionSheet>("Data/Initial Building Definition Sheet"));
+			if(buildingDefinitionSheet == null)
+			{
+				Debug.LogError("Cannot construct buildings, cannot fetch the initial building definition sheet.");
+				return;
+			}
+
+			/* Scene */
+			SceneManager.activeSceneChanged += OnSceneChanged;
+#if DEBUG
+			// Use initial game data if not starting from the menu scene.
+			if(SceneManager.GetActiveScene().buildIndex != 0)
+				CreateNewGameInstance();
+#endif
+		}
+		#endregion
+
+		#region Built-in resources
+		static BuildingDefinitionSheet buildingDefinitionSheet;
+		public static List<BuildingDefinition> BuildingDefinitions => buildingDefinitionSheet?.buildingDefinitions;
+		public static bool FindBuildingDefinitionByType(string type, out BuildingDefinition definition)
+		{
+			definition = BuildingDefinitions.Find(d => d.id == type);
+			return definition != null;
+		}
+		#endregion
+
+		#region Local data
+		#region Settings
+		static readonly string settingsPath = Path.Combine(Application.persistentDataPath, "settings.json");
+		static GameSettings settings = null;
+		static readonly Action onSettingsChanged;
+
+		static void EnsureSettingsPath()
+		{
+			if(Directory.Exists(settingsPath))
+				Directory.Delete(settingsPath, true);
+			if(!File.Exists(settingsPath))
+				File.Create(settingsPath);
+		}
+
+		static void ReadSettings()
+		{
+			if(File.Exists(settingsPath))
+			{
+				try
+				{
+					settings = JsonUtility.FromJson<GameSettings>(File.ReadAllText(settingsPath));
+				}
+				catch(Exception e)
+				{
+					Debug.LogWarning($"Failed to read game settings at {settingsPath}.");
+					Debug.LogError(e);
+					settings = null;
+				}
+			}
+
+			if(settings == null)
+			{
+				settings = new();
+				WriteSettings();
+			}
+
+			onSettingsChanged?.Invoke();
+		}
+
+		public static void WriteSettings()
+		{
+			try
+			{
+				EnsureSettingsPath();
+				File.WriteAllText(settingsPath, JsonUtility.ToJson(settings));
+			}
+			catch(Exception e)
+			{
+				Debug.LogWarning($"Failed to write game settings to {settingsPath}.");
+				Debug.LogError(e);
+			}
+		}
+		#endregion
+
+		#region Savegames
+		static readonly string savegamesDir = Path.Combine(Application.persistentDataPath, "savegames");
+		static readonly List<string> savegamePaths = new();
+		static readonly Action onSavegamesChanged;
+
+		static void EnsureSavegamesDir()
+		{
+			if(File.Exists(savegamesDir))
+				File.Delete(savegamesDir);
+			if(!Directory.Exists(savegamesDir))
+				Directory.CreateDirectory(savegamesDir);
+		}
+
+		static void ReadSavegamePaths()
+		{
+			EnsureSavegamesDir();
+			foreach(string path in Directory.EnumerateFiles(savegamesDir))
+			{
+				if(savegamePaths.Contains(path))
+					continue;
+				savegamePaths.Add(path);
+			}
+			onSavegamesChanged?.Invoke();
+		}
+
+		public static Savegame ReadSavegame(string path)
+		{
+			try
+			{
+				var savegame = JsonUtility.FromJson<Savegame>(File.ReadAllText(path));
+				return savegame;
+			}
+			catch(Exception e)
+			{
+				Debug.LogWarning($"Failed to read the savegame at {path}.");
+				Debug.LogError(e);
+				return null;
+			}
+		}
+
+		public static void WriteSavegame(string path, Savegame savegame)
+		{
+			try
+			{
+				if(!File.Exists(path))
+					File.Create(path);
+				File.WriteAllText(path, JsonUtility.ToJson(savegame));
+				onSavegamesChanged?.Invoke();
+			}
+			catch(Exception e)
+			{
+				Debug.LogWarning($"Failed to write savegame \"{savegame.name}\" to {path}.");
+				Debug.LogError(e);
+			}
+		}
+		#endregion
+		#endregion
+
+		#region Scene
+		public static void SwitchScene(string sceneName)
+		{
+			SceneManager.LoadScene(sceneName);
+		}
+
+		static void OnSceneChanged(Scene previous, Scene after)
+		{
+			// Update environmental lighting.
+			DynamicGI.UpdateEnvironment();
+		}
+		#endregion
+
+		#region Game instance
+		public static GameInstance Instance => GameInstance.Instance;
+
+		public static void StartNewGame()
+		{
+			if(Instance != null)
+			{
+				Debug.LogError("A game instance is already running, cannot start new game.");
+				return;
+			}
+
+			CreateNewGameInstance();
+
+			SwitchScene("Polis");
+		}
+
+		private static void CreateNewGameInstance()
+		{
+			GameObject go = new("Game Instance");
+			go.AddComponent<GameInstance>();
+			Instance.Data = Resources.Load<GameDataSO>("Data/Initial Game Data").gameData;
+		}
+
+		public static void StopCurrentGame()
+		{
+			if(Instance == null)
+			{
+				Debug.LogError("No game instance is currently running, cannot stop current game.");
+				return;
+			}
+
+			// TODO: Save the game.
+
+			// Destroy the current game instance.
+			UnityEngine.Object.Destroy(Instance);
+
+			SwitchScene("Start Menu");
+		}
+		#endregion
+
+		#region Pause menu
+		static PauseMenu pauseMenu;
+		public static void OpenPauseMenu()
+		{
+			if(Instance == null)
+			{
+				Debug.LogWarning("Pause menu can only be opened when a game instance is running.");
+				return;
+			}
+			if(pauseMenu != null)
+				return;
+
+			pauseMenu = UnityEngine.Object.Instantiate(Resources.Load<GameObject>("UI/Pause Menu")).GetComponent<PauseMenu>();
+			Time.timeScale = 0.0f;
+		}
+
+		public static void ClosePauseMenu()
+		{
+			if(pauseMenu == null)
+				return;
+
+			UnityEngine.Object.Destroy(pauseMenu.gameObject);
+			pauseMenu = null;
+			Time.timeScale = 1.0f;
+		}
+		#endregion
+	}
+}
---
+++ Assets/Scripts/Polis/ConstructModal.cs
@@ -0,0 +1,178 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace LongLiveKhioyen
+{
+	public class ConstructModal : MonoBehaviour
+	{
+		public MayorMode mayorMode;
+		int orientation;
+		Polis Polis => Polis.Instance;
+		public LayoutGroup constructOptionsUi;
+
+		#region Life cycle
+		void OnEnable()
+		{
+			GenerateUi();
+			SelectedBuildingType = null;
+			ShowCostPreview = false;
+			Polis.onEconomyDataChanged += OnEconomyDataChanged;
+		}
+
+		void OnDisable()
+		{
+			SelectedBuildingType = null;
+		}
+
+		void OnEconomyDataChanged()
+		{
+			if(selectedBuildingType != null)
+			{
+				if(!(selectedBuildingType.cost <= Polis.ControlledData.economy))
+					SelectedBuildingType = null;
+			}
+		}
+		#endregion
+
+		#region Input handlers
+		protected void OnRotateBuilding()
+		{
+			orientation = (orientation + 1) % 4;
+			UpdatePreviewModelPose();
+		}
+
+		protected void OnDrag()
+		{
+			UpdatePreviewModelPose();
+		}
+		#endregion
+
+		#region UI
+		void GenerateUi()
+		{
+			List<Transform> children = new();
+			for(int i = 0; i < constructOptionsUi.transform.childCount; ++i)
+				children.Add(constructOptionsUi.transform.GetChild(i));
+			foreach(var child in children)
+				Destroy(child.gameObject);
+
+			var cardTemplate = Resources.Load<GameObject>("UI/Polis/Construct Option Card");
+			foreach(var definition in GameManager.BuildingDefinitions)
+			{
+				var card = Instantiate(cardTemplate).GetComponent<ConstructOptionCard>();
+				card.buildingDefinition = definition;
+				card.transform.SetParent(constructOptionsUi.transform, false);
+
+				card.onSelected += OnConstructionCardSelected;
+				card.onHovered += OnConstructionCardHovered;
+				card.onUnhovered += OnConstructionCardUnhovered;
+			}
+			constructOptionsUi.CalculateLayoutInputHorizontal();
+		}
+
+		void OnConstructionCardSelected(ConstructOptionCard card)
+		{
+			SelectedBuildingType = card.buildingDefinition.id;
+		}
+
+		void OnConstructionCardHovered(ConstructOptionCard card)
+		{
+			hoveredBuildingType = card.buildingDefinition;
+			ShowCostPreview = true;
+		}
+
+		void OnConstructionCardUnhovered(ConstructOptionCard card)
+		{
+			ShowCostPreview = false;
+		}
+		#endregion
+
+		#region Selection
+		BuildingDefinition selectedBuildingType, hoveredBuildingType;
+		GameObject previewModel;
+		public string SelectedBuildingType
+		{
+			get => selectedBuildingType?.id;
+			set
+			{
+				if(previewModel != null)
+				{
+					Destroy(previewModel);
+					previewModel = null;
+				}
+
+				if(!GameManager.FindBuildingDefinitionByType(value, out selectedBuildingType))
+					return;
+
+				if(selectedBuildingType.model == null)
+					return;
+
+				orientation = selectedBuildingType.defaultOrientation;
+				previewModel = Instantiate(selectedBuildingType.model);
+				previewModel.transform.SetParent(Polis.transform, false);
+				UpdatePreviewModelPose();
+			}
+		}
+
+		void UpdatePreviewModelPose()
+		{
+			if(previewModel == null)
+				return;
+
+			if(!Polis.ScreenToGround(mayorMode.PointerScreenPosition, out var groundPos))
+			{
+				previewModel.SetActive(false);
+				return;
+			}
+
+			var gridPosition = Polis.grid.WorldToCell(groundPos);
+			Polis.PositionBuilding(previewModel.transform, selectedBuildingType.pivot, Polis.GridToMap(gridPosition), orientation);
+		}
+		#endregion
+
+		#region Cost preview
+		[SerializeField] CostPreviewPanel costPreviewPanel;
+		bool ShowCostPreview
+		{
+			get => costPreviewPanel.gameObject.activeSelf;
+			set
+			{
+				if(hoveredBuildingType == null)
+					value = false;
+				if(value == ShowCostPreview)
+					return;
+
+				costPreviewPanel.gameObject.SetActive(value);
+				if(value)
+					costPreviewPanel.UpdateCostData(hoveredBuildingType.cost);
+			}
+		}
+		#endregion
+
+		#region Actions
+		protected void Interact()
+		{
+			TryPlaceBuilding();
+		}
+
+		void TryPlaceBuilding()
+		{
+			if(selectedBuildingType == null)
+				return;
+			if(!Polis.ScreenToGround(mayorMode.PointerScreenPosition, out Vector3 groundPosition))
+				return;
+			if(!Polis.TryCostResource(selectedBuildingType.cost))
+			{
+				Debug.LogWarning(
+					$"Not enough resources to build a {selectedBuildingType.name}!\n" +
+					$"Required: {selectedBuildingType.cost}, current: {Polis.ControlledData.economy}."
+				);
+				return;
+			}
+			var gridPosition = Polis.grid.WorldToCell(groundPosition);
+			Polis.ConstructBuilding(selectedBuildingType.id, Polis.GridToMap(gridPosition), orientation);
+		}
+		#endregion
+	}
+}
