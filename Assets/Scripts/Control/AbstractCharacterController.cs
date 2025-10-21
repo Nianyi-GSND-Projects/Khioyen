09371e96f7eabf6dcd6b364729b0b7d7f49b33f1
Nianyi Wang <wangnianyi2001@outlook.com>
2025-10-21 00:29:13 -0400

    Horse walks

--- Assets/Scripts/Control/AbstractCharacterController.cs
+++ Assets/Scripts/Control/AbstractCharacterController.cs
@@ -4,11 +4,41 @@ namespace LongLiveKhioyen
 {
 	public abstract class AbstractCharacterController : MonoBehaviour
 	{
+		[SerializeField] Animator animator;
+
 		[Min(0)] public float moveSpeed = 30;
 		[Min(0)] public float lateralSpeed = 40;
 
-		public virtual float ForwardMoveInput { get; set; } = 0;
-		public virtual float LateralMoveInput { get; set; } = 0;
+		bool isMoving = false;
+		public bool IsMoving
+		{
+			get => isMoving;
+			set
+			{
+				isMoving = value;
+				if(animator)
+					animator.SetBool("Is Moving", isMoving);
+			}
+		}
+
+		Vector2 moveInput;
+		public virtual float ForwardMoveInput {
+			get => moveInput.y;
+			set
+			{
+				moveInput.y = value;
+				IsMoving = moveInput.sqrMagnitude != 0;
+			}
+		}
+		public virtual float LateralMoveInput
+		{
+			get => moveInput.x;
+			set
+			{
+				moveInput.x = value;
+				IsMoving = moveInput.sqrMagnitude != 0;
+			}
+		}
 
 		public virtual void Teleport(Vector3 position)
 		{
